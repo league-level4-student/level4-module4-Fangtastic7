6c4b359221f336cfffad04ac5172fbdf0d3deea6
Fangtastic7 <mwfang7@gmail.com>
2020-04-10 18:56:01 -0700

    EncapsulateTest

---
+++ src/_01_introduction_to_encapsulation/EncapsulateTest.java
@@ -0,0 +1,63 @@
+package _01_introduction_to_encapsulation;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class EncapsulateTest {
+EncapsulateTheData obj = new EncapsulateTheData();
+
+@Test
+public void testitemsReceived() {
+	try {
+		obj.setitemsReceived(5);
+		assertEquals(5, obj.getitemsReceived());
+		obj.setitemsReceived(-1);
+		assertEquals(0, obj.getitemsReceived());
+		//fail("There is an error ");
+	}catch(Exception e) {
+		e.printStackTrace();
+		System.out.println("No value set.");
+	}
+}
+
+@Test
+public void testdegreesTurned() {
+	try {
+		obj.setdegreesTurned(5f);
+		assertEquals(5f, obj.getdegreesTurned());
+		obj.setdegreesTurned(35.5f);
+		assertEquals(35.5, obj.getdegreesTurned());
+		obj.setdegreesTurned(-1.0f);
+		assertEquals(0f, obj.getdegreesTurned());
+	}catch(Exception e) {
+		e.printStackTrace();
+		System.out.println("not a float");
+	}
+}
+
+@Test
+public void testnomenclature() {
+	try {
+		obj.setnomenclature("bob");
+		assertEquals("bob", obj.getnomenlature());
+		obj.setnomenclature("");
+		assertEquals(" ", obj.getnomenlature());
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+
+@Test
+public void testmemberOb() {
+	try {
+		obj.setmemberOb(1);
+		assertEquals(1, obj.getmemberOb());
+		obj.setmemberOb("bob");
+		assertEquals( (Object) obj.getmemberOb(), obj.getmemberOb());
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+}
--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -1,5 +1,12 @@
 package _01_introduction_to_encapsulation;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.ItemSelectable;
+
+import org.junit.Test;
+
 /*
  * Encapsulation is a way of protecting the data in a class from being
  * unintentionally altered from another class.
@@ -15,21 +22,63 @@ package _01_introduction_to_encapsulation;
  * 
  * */
 
-
 public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
-	
-	//2. Create a new JUnit Test case and write tests to verify that 
-	//   the member variables' getters and setters are working
-	
-	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	protected String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	protected Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
-	
-	
-	public static void main(String[] args) {
-		
+	// 1. Encapsulate the member variables.
+	// Add restrictions to the setters according to the comment.
+
+	// 2. Create a new JUnit Test case and write tests to verify that
+	// the member variables' getters and setters are working
+
+	private int itemsReceived; // must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; // must be locked between 0.0 and 360.0 inclusive. If it is out of bounds, then
+									// set the degrees to 0.
+	private String nomenclature = " "; // must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; // must not be a String. If it is a String, set it equal to a new Object();
+
+	int getitemsReceived() {
+		return itemsReceived;
 	}
+
+	public void setitemsReceived(int item) {
+		if (item < 0) {
+			item = 0;
+		}
+		this.itemsReceived = item;
+	}
+
+	float getdegreesTurned() {
+		return degreesTurned;
+	}
+
+	public void setdegreesTurned(float degrees) {
+		if (degrees < 0.0f || degrees > 360.0f) {
+			System.out.println(degrees);
+			degrees = 0.0f;
+		}
+		this.degreesTurned = degrees;
+	}
+	
+	String getnomenlature() {
+		return nomenclature;
+	}
+	
+	public void setnomenclature(String word) {
+		if(word.equals("")) {
+			word = " ";
+		}
+		this.nomenclature = word;
+	}
+	
+	Object getmemberOb() {
+		return memberObj;
+	}
+	
+	public void setmemberOb(Object var) {
+		if(var instanceof String ) {
+			var = new Object();
+		}
+		this.memberObj = var;
+	}
+
+
 }
